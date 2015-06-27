7df8a7e5585979e783ce4c1068baf16693534baf
Quanzheng Long <qlong@purdue.edu>
2015-06-27 10:13:30 -0700

    db works

--- src/db.js
+++ src/db.js
@@ -3,129 +3,171 @@
 'use strict';
 var AWS = require("aws-sdk");
 
+/**
+//how to use?
+    db.putRecipe(recipes[recipeName],function(){
+        db.getRecipe(function(recipe){
+                console.log("your recipe",recipe);
+                db.putSize(""+Date.now(),function(){
+                console.log("succ put size");
+                db.getSize(function(size){
+                    console.log("succ get size",size);
+                    db.putStep(""+Date.now(),function(){
+                        console.log("succ put step");
+                        db.getStep(function(step){
+                            console.log("succ get step",step);
+                            response.ask("Sorry, no idea how to make  Any other requests?");
+                        });
+                    });
+                    
+                });
+
+            });
+            
+        });
+        
+    });
+**/
+
 var db = (function () {
     var dynamodb = new AWS.DynamoDB({apiVersion: '2012-10-17'});
+    var oneId = "qlong_chef";
 
-     DB.prototype = {
+    return{
 
-        oneId : "qlong_chef",
-
-        "putRecipe" : function(recipe,callback){
-            dynamodb.putItem({
-                TableName: 'chef',
-                Item: {
-                    rid: {
-                        S: Game.prototype.oneId
-                    },
+        // recipe
+        putRecipe : function(recipe,callback){
+            console.log("put recipe:",recipe);
+            var str = JSON.stringify(recipe);
+            var putS = {
+                AttributeUpdates: {
                     recipe: {
-                        S: recipe
+                        Action : "PUT",
+                        Value: {
+                            S: str
+                        }
+                    }
+                },
+                TableName: 'chef',
+                Key : {
+                    rid: {
+                        S: 'qlong_chef'
                     }
                 }
-            }, function (err, recipe) {
-                if (err) {
-                    console.log(err, err.stack,recipe);
-                }
-                if (callback) {
-                    callback();
-                }
+            };
+            console.log("putS",putS);
+            dynamodb.updateItem(putS, function (err, recipe) {
+                console.log("succ put recipe");
+                callback();
             });
         },
 
-        "getRecipe" : function(callback){
-            dynamodb.getItem({
+        getRecipe : function(callback){
+            console.log("here to get recipe");
+            var getR = {
                 TableName: 'chef',
-                Item: {
+                Key : {
                     rid: {
-                        S: Game.prototype.oneId
+                        S: 'qlong_chef'
                     }
                 }
-            }, function (err,retData) {
-                if (err) {
-                    console.log(err, err.stack);
-                }
-                if (callback) {
-                    callback(retData.recipe);
-                }
+            };
+            console.log("request to db",getR);
+            dynamodb.getItem(getR, function (err,retData) {
+                console.log("got u",retData);
+                callback(retData.Item.recipe.S);
             });
         },
 
-        "updateSize" : function(size,callback){
-            dynamodb.putItem({
-                TableName: 'chef',
-                Item: {
-                    rid: {
-                        S: Game.prototype.oneId
-                    },
+
+        // size
+        putSize : function(size,callback){
+            console.log("put size:",size);
+            var str = JSON.stringify(size);
+            var putS = {
+                AttributeUpdates: {
                     size: {
-                        S: size
+                        Action : "PUT",
+                        Value: {
+                            S: str
+                        }
+                    }
+                },
+                TableName: 'chef',
+                Key : {
+                    rid: {
+                        S: 'qlong_chef'
                     }
                 }
-            }, function (err, size) {
-                if (err) {
-                    console.log(err, err.stack,size);
-                }
-                if (callback) {
-                    callback();
-                }
+            };
+            console.log("putS",putS);
+            dynamodb.updateItem(putS, function (err, recipe) {
+                console.log("succ put size");
+                callback();
             });
         },
 
-        "getSize" : function(callback){
-            dynamodb.getItem({
+        getSize : function(callback){
+            console.log("here to get size");
+            var getR = {
                 TableName: 'chef',
-                Item: {
+                Key : {
                     rid: {
-                        S: Game.prototype.oneId
+                        S: 'qlong_chef'
                     }
                 }
-            }, function (err,retData) {
-                if (err) {
-                    console.log(err, err.stack);
-                }
-                if (callback) {
-                    callback(retData.size);
-                }
+            };
+            console.log("request to db",getR);
+            dynamodb.getItem(getR, function (err,retData) {
+                console.log("got u",retData);
+                callback(retData.Item.size.S);
             });
         },
 
-        "updateStep" : function(step,callback){
-            dynamodb.putItem({
-                TableName: 'chef',
-                Item: {
-                    rid: {
-                        S: Game.prototype.oneId
-                    },
+
+        // step
+        putStep : function(step,callback){
+            console.log("put step:",step);
+            var str = JSON.stringify(step);
+            var putS = {
+                AttributeUpdates: {
                     step: {
-                        S: step
+                        Action : "PUT",
+                        Value: {
+                            S: str
+                        }
+                    }
+                },
+                TableName: 'chef',
+                Key : {
+                    rid: {
+                        S: 'qlong_chef'
                     }
                 }
-            }, function (err, step) {
-                if (err) {
-                    console.log(err, err.stack,step);
-                }
-                if (callback) {
-                    callback();
-                }
+            };
+            console.log("putS",putS);
+            dynamodb.updateItem(putS, function (err, recipe) {
+                console.log("succ put step");
+                callback();
             });
         },
 
-        "getStep" : function(callback){
-            dynamodb.getItem({
+        getStep : function(callback){
+            console.log("here to get step");
+            var getR = {
                 TableName: 'chef',
-                Item: {
+                Key : {
                     rid: {
-                        S: Game.prototype.oneId
+                        S: 'qlong_chef'
                     }
                 }
-            }, function (err,retData) {
-                if (err) {
-                    console.log(err, err.stack);
-                }
-                if (callback) {
-                    callback(retData.step);
-                }
+            };
+            console.log("request to db",getR);
+            dynamodb.getItem(getR, function (err,retData) {
+                console.log("got u",retData);
+                callback(retData.Item.step.S);
             });
-        },
+        }
     };
 
 })();
